90e6d0d766b055fd432e2b1d2ecd1352f203c2ab
srikanthgadadasu <50926239+srikanthgadadasu@users.noreply.github.com>
2020-05-27 15:34:50 -0400

    Update csvread.go

--- src/readcsv/csvread.go
+++ src/readcsv/csvread.go
@@ -1,10 +1,13 @@
+// Copyright 2020 The Nadi Authors. All rights reserved.
+// Use of this source code is not permitted
+
 package csvread
 
 import(
 	"github.com/srikanthgadadasu/nadi/src/readcsv/functions"
 )
 
-// Read the input csv file and converts to dataframe using the above schema
+// Read the input csv file and converts to dataframe using schema
 
 func readCsv(){
 	functions.Csvtodataframe()
