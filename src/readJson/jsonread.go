a126627e39d4b2c370eddb80ca14ba1a00c1c3f6
srikanthgadadasu <50926239+srikanthgadadasu@users.noreply.github.com>
2020-05-27 15:30:00 -0400

    Update and rename main.go to jsonread.go

---
+++ src/readJson/jsonread.go
@@ -0,0 +1,16 @@
+// Copyright 2020 The Nadi Authors. All rights reserved.
+// Use of this source code is not permitted
+
+package readjson
+
+import(
+   "fmt"
+   "os"
+)
+
+//It has to read the JSON file and returns rows array
+
+func jsonRead{
+
+
+}
