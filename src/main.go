e05e7670a7e3a16cfdb9cf513db243f5d334e145
Srikanth Gadadasu <sriyan@Srikanths-MBP.T-mobile.com>
2020-05-28 00:25:53 -0400

    read csv

--- src/main.go
+++ src/main.go
@@ -1,9 +1,13 @@
 package main
 
 import(
+	"fmt"
 	"github.com/srikanthgadadasu/nadi/src/readcsv"
 )
 
 func main(){
-	readcsv.Readcsvfile()
+	Ipfile := "/Users/sriyan/Documents/nadi/udemy_courses.csv"
+	rows := readcsv.Csvtodataframe(Ipfile)
+	fmt.Println(rows[0]) //sample record
+	fmt.Println(rows[0].Course_title)
 }
--- src/readcsv/csvread.go
+++ src/readcsv/csvread.go
@@ -4,10 +4,82 @@
 package readcsv
 
 import(
-	"github.com/srikanthgadadasu/nadi/src/readcsv/functions"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"io"
+	"time"
+	"strconv"
+	"bufio"
 )
 
-//Readcsvfile read the input csv file and converts to dataframe using schema
-func Readcsvfile(){
-	functions.Csvtodataframe()
+//Iptype is input csv file schema
+type Iptype struct{
+	Course_id string
+	Course_title string
+	Url	string
+	Is_paid	bool
+	Price float64
+	Num_subscribers int	
+	Num_reviews	int
+	Num_lectures int
+	Level string
+	Content_duration float64	
+	Published_timestamp time.Time
+	Subject string
+}
+
+//Csvtodataframe function converts csv data into data frame format using the schema
+func Csvtodataframe(Ipfile string) []Iptype {
+
+	csvfile, err := os.Open(Ipfile)
+	checkerror(err)
+	defer csvfile.Close()
+	
+	// Skip first row (line)
+    row1, err := bufio.NewReader(csvfile).ReadSlice('\n')
+    checkerror(err)
+	_, err = csvfile.Seek(int64(len(row1)), io.SeekStart)
+	checkerror(err)
+	
+	//read remaining lines
+	data := csv.NewReader(csvfile)
+	rawdata, err := data.ReadAll()
+	checkerror(err)
+
+	var line Iptype
+	var lines []Iptype
+	layout := time.RFC3339
+
+	for _, record := range rawdata {
+		line.Course_id = record[0]
+		line.Course_title = record[1]
+		line.Url = record[2]
+		line.Is_paid, err = strconv.ParseBool(record[3])
+		checkerror(err)
+		line.Price, err = strconv.ParseFloat(record[4], 64)
+		checkerror(err)
+		line.Num_subscribers, err = strconv.Atoi(record[5])
+		checkerror(err)
+		line.Num_reviews, err = strconv.Atoi(record[6])
+		checkerror(err)
+		line.Num_lectures, err = strconv.Atoi(record[7])
+		checkerror(err)
+		line.Level = record[8]
+		line.Content_duration, err = strconv.ParseFloat(record[9], 64)
+		checkerror(err)
+		line.Published_timestamp, err = time.Parse(layout, record[10])
+		checkerror(err)
+		line.Subject = record[11]
+
+		lines = append(lines, line)
+	}
+	return lines
+}
+
+func checkerror(err error){
+	if err != nil{
+		fmt.Println("Error: ",err)
+		os.Exit(1)
+	}
 }
