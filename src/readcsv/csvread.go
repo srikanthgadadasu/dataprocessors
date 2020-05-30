45f089084d2e73a12e3d4ff831a459cad6d09712
Nadi Data <50926239+nadidata@users.noreply.github.com>
2020-05-29 22:45:32 -0400

    added more comments

--- src/readcsv/csvread.go
+++ src/readcsv/csvread.go
@@ -1,5 +1,6 @@
-// Copyright 2020 The Nadi Authors. All rights reserved.
-// Use of this source code is not permitted
+// Copyright 2020 The Nadi Data Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package readcsv
 
@@ -13,7 +14,8 @@ import(
 	"bufio"
 )
 
-//Iptype is input csv file schema
+//Iptype is input csv file schema (created for sample file(udemy_courses.csv) located in testfiles directory). 
+//This needs to be moved to another file and pass the file and pass it as parameter to Csvtodataframe function
 type Iptype struct{
 	Course_id string
 	Course_title string
@@ -52,9 +54,13 @@ func Csvtodataframe(Ipfile string) []Iptype {
 	layout := time.RFC3339
 
 	for _, record := range rawdata {
+		//Below columns assignment must be generic, as column names come from file and assignments should be generic
 		line.Course_id = record[0]
+		checkerror(err)
 		line.Course_title = record[1]
+		checkerror(err)
 		line.Url = record[2]
+		checkerror(err)
 		line.Is_paid, err = strconv.ParseBool(record[3])
 		checkerror(err)
 		line.Price, err = strconv.ParseFloat(record[4], 64)
@@ -66,11 +72,13 @@ func Csvtodataframe(Ipfile string) []Iptype {
 		line.Num_lectures, err = strconv.Atoi(record[7])
 		checkerror(err)
 		line.Level = record[8]
+		checkerror(err)
 		line.Content_duration, err = strconv.ParseFloat(record[9], 64)
 		checkerror(err)
 		line.Published_timestamp, err = time.Parse(layout, record[10])
 		checkerror(err)
 		line.Subject = record[11]
+		checkerror(err)
 
 		lines = append(lines, line)
 	}
