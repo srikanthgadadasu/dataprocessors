9fdca21c9d7c62b05d0bbc3de9f95440a5b4e4b9
Nadi Data <50926239+nadidata@users.noreply.github.com>
2020-05-29 23:09:33 -0400

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,12 +1,16 @@
+// Copyright 2020 The Nadi Data Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import(
 	"fmt"
-	"github.com/srikanthgadadasu/nadi/src/readcsv"
+	"github.com/nadidata/DataProcessors/src/readcsv"
 )
 
 func main(){
-	Ipfile := "github.com/srikanthgadadasu/nadi/blob/master/udemy_courses.csv"
+	Ipfile := "testfiles/udemy_courses.csv"
 	rows := readcsv.Csvtodataframe(Ipfile)
 	fmt.Println(rows[0]) //sample record
 	fmt.Println(rows[0].Course_title)
